fc4c98a72e675788a791497c9e603024ec96d8bc
luciano ereu <71347892+lucianoereu@users.noreply.github.com>
2022-08-18 21:54:51 -0300

    Update main.js

--- Js/main.js
+++ Js/main.js
@@ -1,4 +1,5 @@
-//variable
+
+//variables
 let nombre;
 let anoNacimiento;
 const anoActual = 2022
@@ -7,23 +8,41 @@ nombre = prompt ('ingrese su nombre ');
 anoNacimiento = prompt (nombre +' ingrese su año de nacimento ');
 let numero = parseInt(anoNacimiento);
 let edad = anoActual - numero;
-console.log(typeof(numero));
+//console.log(typeof(numero));
 console.log(nombre + ' tu edad es ' + edad);
+
+//verificacion de numero en el propmt
+/*while(numero!=Number){
+if ( numero = isNaN){
+    alert('ingrese un numero')
+    anoNacimiento = prompt (nombre +' ingrese su año de nacimento ');
+    numero = parseInt(anoNacimiento);
+};};*/
 
 
 if(edad >= 18){
     console.log(nombre + ' ' +'tu puedes votar');
 }else{
-    console.log(nombre + ' ' + 'no eres mayor de edad, no pedes votar')
+    console.log(nombre + ' ' + 'no eres mayor de edad, no puedes votar')
 };
 
-let usuario = prompt('ingrese usuario');
-let password = prompt('ingrese contraseña');
 let pass = 1234;
 let user = 'lu';
+let usuario;
+let contador= 0;
 
-if ((usuario != user) || (password != pass)){
-    console.log('usuario nop');
-}else{
-    console.log('hola lu');
-}
+do{
+    usuario = prompt('ingrese el usuario correspondiente');
+    let password = prompt('ingrese contraseña');
+
+    if ( user === usuario && pass == password){
+        console.log('hola '+ user + ',bienvenido de vuelta!')
+        usuario=user;
+    }
+    contador++
+
+} while(user!=usuario && contador < 3)
+
+if (contador==3){
+    console.log('no puedes ingresar '+ usuario + ';no es tu cuenta!');
+};
